b02cd5a44cc9f80baa57555a0b31c7bd1f9f0913
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-03-22 01:20:22 +0300

    22 Mart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CarComponent } from './components/car/car.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { RentalComponent } from './components/rental/rental.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaviComponent,
+    BrandComponent,
+    ColorComponent,
+    CarComponent,
+    CustomerComponent,
+    RentalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/car.ts
@@ -0,0 +1,9 @@
+export interface Car {
+  id: number;
+  brandId: number;
+  colorId: number;
+  model: string;
+  modelYear: string;
+  dailyPrice: number;
+  description: string;
+}
