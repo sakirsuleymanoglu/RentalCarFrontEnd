e1a856225e0ad932cd0f2e94810a6697c65d2e5c
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-03-25 21:47:53 +0300

    25 mart

---
+++ src/app/models/rentalDtoResponseModel.ts
@@ -0,0 +1,6 @@
+import { RentalDto } from "./rentalDto";
+import { ListResponseModel } from "./listResponseModel";
+
+export interface RentalDtoResponseModel extends ListResponseModel<RentalDto>{
+
+}
--- src/app/services/rental.service.ts
+++ src/app/services/rental.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RentalDtoResponseModel } from '../models/rentalDtoResponseModel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RentalService {
+  private apiUrlByGetAllDetails =
+    'https://localhost:44394/api/Cars/getallwithdetails';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getDetailsOfRentals(): Observable<RentalDtoResponseModel>{
+    return this.httpClient.get<RentalDtoResponseModel>(this.apiUrlByGetAllDetails);
+  }
 }
