52ab641f58f778bed6d14fdfff16dee4810019e7
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-03-31 22:31:48 +0300

    brand add page

--- src/app/components/brand-add/brand-add.component.ts
+++ src/app/components/brand-add/brand-add.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators} from '@angular/forms'
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Brand } from 'src/app/models/brand';
+import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-brand-add',
   templateUrl: './brand-add.component.html',
-  styleUrls: ['./brand-add.component.css']
+  styleUrls: ['./brand-add.component.css'],
 })
 export class BrandAddComponent implements OnInit {
+  brandAddForm: FormGroup;
+  brand: Brand;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private brandService: BrandService
+  ) {}
 
   ngOnInit(): void {
+    this.createBrandAddForm();
   }
 
+  createBrandAddForm() {
+    this.brandAddForm = this.formBuilder.group({
+      name: ['', Validators.required],
+    });
+  }
+
+  add() {
+    if (this.brandAddForm.valid) {
+      this.brand = Object.assign({}, this.brandAddForm.value);
+      this.brandService.add(this.brand).subscribe((response) => {
+        alert(response.success);
+      });
+    }
+  }
 }
--- src/app/services/brand.service.ts
+++ src/app/services/brand.service.ts
@@ -3,17 +3,24 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Brand } from '../models/brand';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrandService {
 
-  apiUrl = "https://localhost:44394/api/Brands/getall";
+  apiUrl = 'https://localhost:44394/api/';
 
   constructor(private httpClient:HttpClient) { }
 
   getBrands():Observable<ListResponseModel<Brand>>{
-    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl);
+    let newPath = this.apiUrl + 'Brands/getall';
+    return this.httpClient.get<ListResponseModel<Brand>>(newPath);
+  }
+
+  add(brand:Brand):Observable<ResponseModel>{
+    let newPath = this.apiUrl + 'Brands/add';
+    return this.httpClient.post<ResponseModel>(newPath, brand);
   }
 }
