f703b5b5e3fc75bdcdc4162bf0d080dea00e4c19
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2021-03-31 22:07:54 +0300

    31 mart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router';
+import { BrandAddComponent } from './components/brand-add/brand-add.component';
+import { BrandDeleteComponent } from './components/brand-delete/brand-delete.component';
+import { BrandUpdateComponent } from './components/brand-update/brand-update.component';
 import { CarAddComponent } from './components/car-add/car-add.component';
 import { CarDeleteComponent } from './components/car-delete/car-delete.component';
 import { CarUpdateComponent } from './components/car-update/car-update.component';
 import { CarComponent } from './components/car/car.component';
 import { CardtoComponent } from './components/cardto/cardto.component';
+import { ColorAddComponent } from './components/color-add/color-add.component';
+import { ColorDeleteComponent } from './components/color-delete/color-delete.component';
+import { ColorUpdateComponent } from './components/color-update/color-update.component';
 import { PaymentComponent } from './components/payment/payment.component';
 import { RentalAddComponent } from './components/rental-add/rental-add.component';
 
@@ -23,7 +29,13 @@ const routes: Routes = [
   },
   {path:'car/add', component:CarAddComponent},
   {path:'car/delete', component:CarDeleteComponent},
-  {path:'car/update', component:CarUpdateComponent}
+  {path:'car/update', component:CarUpdateComponent},
+  {path:'brand/add', component:BrandAddComponent},
+  {path:'brand/delete', component:BrandDeleteComponent},
+  {path:'brand/update', component:BrandUpdateComponent},
+  {path:'color/add', component:ColorAddComponent},
+  {path:'color/delete', component:ColorDeleteComponent},
+  {path:'color/update', component:ColorUpdateComponent}
 ];
 
 @NgModule({
